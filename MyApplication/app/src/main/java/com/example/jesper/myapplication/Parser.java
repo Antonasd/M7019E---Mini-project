73823a3f0607c7a0ff4ed4adeafacc4ee0640d2d
Kannell <johan.kannel@gmail.com>
2018-05-29 18:41:18 +0200

    Added comments

--- MyApplication/app/src/main/java/com/example/jesper/myapplication/Parser.java
+++ MyApplication/app/src/main/java/com/example/jesper/myapplication/Parser.java
@@ -14,6 +14,7 @@ public class Parser {
     private Document doc = null;
     boolean done = false;
 
+	//"Config" for the URLs where data is to be fetched from.
     private String getURL (String year, String value, boolean forTeam) {
         if(forTeam) {
             return "http://teamplaycup.se/cup/?games&home=kurirenspelen/" + year + "&scope=all&" + value + "&field=";
@@ -22,8 +23,10 @@ public class Parser {
         }
     }
 
+	//Get all matches for the specified year and field/arena, used in screen 3.
     public ArrayList<ArrayList<String>> getMatches(final String year, final String arena) throws IOException {
-
+		
+		//Fetching from a URL shouldn't be run in the main thread.
         Thread getThread = new Thread() {
             public void run() {
                 doc = null;
@@ -56,6 +59,7 @@ public class Parser {
         ArrayList<ArrayList<String>> games = new ArrayList<ArrayList<String>>();
 
         int index = 0;
+		//There is one header with a corresponding table for each day of the season, let's loop through them and get every match.
         for (Element title : titles) {
 
             Elements curGames = tables.get(index).children();
@@ -84,6 +88,7 @@ public class Parser {
         return games;
     }
 
+	//Get a list of all the players in a specific match, used in screen 4
     public ArrayList<ArrayList<String>> getPlayers(final String url) throws IOException {
 
         Thread getThread = new Thread() {
@@ -112,19 +117,21 @@ public class Parser {
 
         done=false;
 
+		//The player's table is always the second table.
         Element playersHtml = doc.getElementsByTag("tbody").get(1);
         ArrayList<ArrayList<String>> players = new ArrayList<ArrayList<String>>();
 
         for(Element player : playersHtml.children()){
             ArrayList<String> newPlayer = new ArrayList<>();
-            newPlayer.add(player.child(0).text());
-            newPlayer.add(player.child(1).text());
+            newPlayer.add(player.child(0).text()); //Name
+            newPlayer.add(player.child(1).text()); //Age
             players.add(newPlayer);
         }
 
         return players;
     }
 
+	//Get the year of brith for a specific group, used to determine if players are overaged.
     public String getAge(final String year, final String group) throws IOException {
 
         Thread getThread = new Thread() {
@@ -150,12 +157,12 @@ public class Parser {
                 e.printStackTrace();
             }
         }
-        Log.v("FREE", "Free from while\n");
         done = false;
 
         String groupString = doc.getElementsByTag("h2").get(0).text();
 
         String age;
+		//Use the first letter to determine if the group is of boys or girls, then get the year of birth.
         if(groupString.substring(0,1).equals("P")) {
             age = groupString.substring(7,9);
         } else {
