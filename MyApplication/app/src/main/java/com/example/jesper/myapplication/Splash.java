9edb496b836dbf1b07764af3b71f04a13a136aeb
Jesper Öhman <jesper.ohman.1@gmail.com>
2018-05-29 17:53:47 +0200

    Update Splash.java

--- MyApplication/app/src/main/java/com/example/jesper/myapplication/Splash.java
+++ MyApplication/app/src/main/java/com/example/jesper/myapplication/Splash.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
 
+    // Creates a empty activity with a background splash image that instantly calls Screen1 whenever it is done
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
